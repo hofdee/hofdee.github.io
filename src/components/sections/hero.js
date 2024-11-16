fa50982a1f95093fd9645dbfa998b2493ccb3dcd
hofdee <mahofadecision@gmail.com>
2024-11-16 17:44:47 +0200

    modified hero section

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -63,7 +63,7 @@ const Hero = () => {
   const three = <h3 className="big-heading">I build things for the web.</h3>;
   const four = (
     <p>
-      I'm a Zimbabwean-based software engineer who specializes in building (and occasionally designing)
+      I'm a Zimbabwean-based web engineer who specializes in building (and occasionally designing)
       exceptional digital experiences. Currently, I'm an cofounder at{' '}
       <a href="https://welodge.co.zw/" target="_blank" rel="noreferrer">
         welodge
